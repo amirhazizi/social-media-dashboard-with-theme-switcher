d05b8bc3632fa3ab7a45233987a731e8b58fd583
amir <amirazizihosein@gmail.com>
2023-03-08 13:30:04 +0330

    input change event

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
 // variables
 const switcher = document.querySelector(".theme-switcher")
+// LOAD event listener
+window.addEventListener("load", () => {
+  document.body.classList.add("light")
+})
+
+// switcher event listener
+switcher.addEventListener("change", () => {
+  if (switcher.checked) {
+    document.body.classList.remove("light")
+    document.body.classList.add("dark")
+  } else {
+    document.body.classList.add("light")
+    document.body.classList.remove("dark")
+  }
+})
